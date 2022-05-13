9f13789e15e885e623bdd0f07883da5ef79bdff7
en.design <45923194+enyamahiroshi@users.noreply.github.com>
2022-05-13 12:57:45 +0900

    ダウンロードページのページネーション追加

--- wp-content/themes/yamatomi/archive-download.php
+++ wp-content/themes/yamatomi/archive-download.php
@@ -26,6 +26,16 @@
       </div>
       <?php endwhile; ?>
       <?php endif; ?>
+
+      <?php // ページネーション
+      the_posts_pagination(
+        array(
+          'mid_size' => 1, // 現在ページの左右に表示するページ番号の数
+          'prev_text' => '', // 「前へ」リンクのテキスト
+          'next_text' => '', // 「次へ」リンクのテキスト
+          'type' => 'list', // 戻り値の指定 (plain/list)
+        )
+      ); ?>
     </div>
   </section>
 
